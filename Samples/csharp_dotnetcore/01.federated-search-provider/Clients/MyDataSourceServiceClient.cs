bcb44d6aa2b7526333e5e46ce3bd026fe51b448e
Alok Goyal <74433080+alok2904@users.noreply.github.com>
2022-06-18 21:45:38 -0700

    Adding code to add OBO exchange

--- Samples/csharp_dotnetcore/01.federated-search-provider/Clients/MyDataSourceServiceClient.cs
+++ Samples/csharp_dotnetcore/01.federated-search-provider/Clients/MyDataSourceServiceClient.cs
@@ -1,11 +1,12 @@
-﻿//---------------------------------------------------------------------------------------------------------------------
-// <copyright file="DynamicsServiceClient.cs" company="Microsoft">
+//---------------------------------------------------------------------------------------------------------------------
+// <copyright file="MyDataSourceServiceClient.cs" company="Microsoft">
 //     Copyright (c) Microsoft. All rights reserved.
 // </copyright>
 //---------------------------------------------------------------------------------------------------------------------
 
 namespace Microsoft.SearchProvider.Bots.Clients
 {
+    using Microsoft.Extensions.Logging;
     using System;
     using System.Net.Http;
     using System.Net.Http.Headers;
@@ -18,19 +19,36 @@ namespace Microsoft.SearchProvider.Bots.Clients
     public class MyDataSourceServiceClient 
     {
         /// <summary>
+        /// Active directory authority URL which is responsible to issue token
+        /// </summary>
+        private const string activeDirAuthority = "https://login.windows.net/microsoft.onmicrosoft.com";
+
+        private const string targateApiResourceId = "https://MyApp.myCompany.com";
+
+        /// <summary>
         /// Performs a query based on the user's request.
         /// </summary>
         /// <param name="query">The user's query text.</param>
         /// <param name="oboToken">The on-behalf-of user token; or null.</param>
+        /// <param name="logger">Logger which can be used to log messages</param>
+        /// <param name="aadTokenResolver">AAD token resolver which will be used to get API token</param>
         /// <param name="token">Cancellation token</param>
         /// <returns>The response information to the user's query.</returns>
-         public async Task<ResponseObject> MyDataSourceSearch(
-             string query,
-             SearchBotAuthenticationToken oboToken = null,
-             CancellationToken token = default)
+        public async Task<ResponseObject> MyDataSourceSearch(
+            string query,
+            SearchBotAuthenticationToken oboToken = null,
+            ILogger logger = null,
+            IAadTokenResolver aadTokenResolver = null,
+            CancellationToken token = default)
         {
             // TODO Run the search to generate your answer.
             string wasTokenFound = oboToken == null ? "not found" : "found";
+            string apiToken = null;
+            if (!string.IsNullOrWhiteSpace(oboToken.Token))
+            {
+                // Exchange OBO token to this app OBO token
+                apiToken = aadTokenResolver.GetOnBehalfOfTokenAsync(activeDirAuthority, targateApiResourceId, oboToken.Token).ConfigureAwait(true).GetAwaiter().GetResult();
+            }
             return new ResponseObject { ResponseText = "This is a dummy response retrieved to this query: " + query + " and the token was " + wasTokenFound };
         }
     }
